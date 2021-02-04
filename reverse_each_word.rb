c0a561c784d190c426aa2e9c546f7b36afbddc96
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-04 04:25:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,9 @@
 def reverse_each_word(sentance)
   array = sentance.split(" ")
-  rev_sentance = (" ")
+  
 
   array.each do |array|
     array.reverse!
-    rev_sentance << array.reverse!
+    
   end
-  rev_sentance.join
-  rev_sentance
 end
